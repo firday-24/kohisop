3030c4f58ae96f4640952dd730d2f72635d6bcd3
dion36144-eng <dion36144@gmail.com>
2026-04-19 16:16:50 +0700

    Makanan.java

--- Makanan.java
+++ Makanan.java
@@ -1,11 +1,25 @@
 public class Makanan extends Menu {
-    
+
     public Makanan(String kode, String nama, int harga) {
         super(kode, nama, harga);
     }
 
     @Override
-    public int maksimalPesan(){
+    public int maksimalPesan() {
         return 2;
+    }
+
+    /**
+     * Aturan pajak makanan:
+     *  - harga > 50  -> 8%
+     *  - harga <= 50 -> 11%
+     */
+    @Override
+    public double hitungPajak() {
+        if (harga > 50) {
+            return harga * 0.08;
+        } else {
+            return harga * 0.11;
+        }
     }
 }
